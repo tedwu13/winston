fe4b9841948ed80e27077f3760e3361c5ff0b6f3
Michael Wang <michaelwang0712@gmail.com>
2019-02-02 16:15:50 -0800

    Removed unnecessary migration file

--- db/schema.rb
+++ db/schema.rb
@@ -13,25 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20190202235005) do
 
-  create_table "article_categories", force: :cascade do |t|
-    t.integer "article_id"
-    t.integer "category_id"
-  end
-
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
