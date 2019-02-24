5b5ffefab6537c4581a1bc4a0d6183d071492c5d
Michael Wang <michaelwang0712@gmail.com>
2019-02-24 15:14:09 -0800

    Removed extra answers code

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,28 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :set_answer, only: [:show, :edit, :update, :destroy]
-
-  # GET /answers
-  # GET /answers.json
-  def index
-    @answers = Answer.all
-  end
-
-  # GET /answers/1
-  # GET /answers/1.json
-  def show
-  end
-
-  # GET /answers/new
-  def new
-    @answer = Answer.new
-  end
-
-  # GET /answers/1/edit
-  def edit
-  end
-
   # POST /answers
-  # POST /answers.json
   def create
     questions_list = params[:questions_list].split(',')
     questions_list.each do |q_idx|
@@ -40,39 +17,4 @@ class AnswersController < ApplicationController
 
     head :ok, content_type: "text/html"
   end
-
-  # PATCH/PUT /answers/1
-  # PATCH/PUT /answers/1.json
-  def update
-    respond_to do |format|
-      if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @answer }
-      else
-        format.html { render :edit }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /answers/1
-  # DELETE /answers/1.json
-  def destroy
-    @answer.destroy
-    respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_answer
-      @answer = Answer.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def answer_params
-      params.fetch(:answer, {})
-    end
 end
