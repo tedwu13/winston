a22a896886078f802762ef56c5ed4326549c81df
Michael Wang <michaelwang0712@gmail.com>
2019-03-16 17:09:16 -0700

    Added an availabilities table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190310220326) do
+ActiveRecord::Schema.define(version: 20190310224446) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20190310220326) do
   end
 
   add_index "answers", ["user_id"], name: "index_answers_on_user_id"
+
+  create_table "availabilities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "available_at"
+  end
+
+  add_index "availabilities", ["user_id"], name: "index_availabilities_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string "question_text"
