1052cbc0d83eae7483d43d916f06a6e0898eee52
Ted Wu <tedwu2016@u.northwestern.edu>
2019-06-24 23:23:59 -0700

    add fonts to asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-#https://stackoverflow.com/questions/19409600/rails-4-why-are-fonts-not-loading-in-production
+
+# Adding fonts
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
