8f2cfe5aff7eb0e03441d8c24c4adcd753c09eb2
Michael Wang <michaelwang0712@gmail.com>
2019-06-30 02:13:01 -0700

    Added functionality for form parts 1 and 2

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,26 +1,34 @@
 class AnswersController < ApplicationController
+  QUESTION_2_CHOICES = ['northeast', 'midwest', 'southeast', 'southwest', 'west', 'no_preference']
+
   # POST /answers
   def create
-    questions_list = params[:questions_list].split(',')
-    questions_list.each do |q_idx|
-      answer = params[q_idx.to_s]
-      unless answer.empty?
-        saved_answer = Answer.find_by(user_id: current_user.id, question_id: q_idx)
-        if saved_answer.present?
-          saved_answer.update(answer_text: answer)
-          saved_answer.save!
-        else
-          Answer.create(user_id: current_user.id, question_id: q_idx, answer_text: answer)
+    puts params
+
+    question_id = params[:question_id]
+    if question_id.to_i == 2
+      answer = []
+      QUESTION_2_CHOICES.each do |choice|
+        if params.key?(choice)
+          answer.push(choice)
         end
+      end
+      answer = answer.join(',')
+    else
+      answer = params[question_id]
+    end
+
+    unless answer.empty?
+      saved_answer = Answer.find_by(user_id: current_user.id, question_id: question_id)
+      if saved_answer.present?
+        saved_answer.update(answer_text: answer)
+        saved_answer.save!
+      else
+        Answer.create(user_id: current_user.id, question_id: question_id, answer_text: answer)
       end
     end
 
-    if params[:survey_completed] == 'true'
-      current_user.update(survey_completed: true)
-    else
-      current_user.update(survey_completed: false)
-    end
-
-    head :ok, content_type: "text/html"
+    next_question_id = question_id.to_i + 1
+    redirect_to "/survey/#{next_question_id}"
   end
 end
--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,24 +1,24 @@
 class SurveyController < ApplicationController
   DEFAULT_QUESTIONS = [1,2,3,4,5]
-  def index
-    @questions_list = DEFAULT_QUESTIONS.join(",")
-    @qna = []
-    DEFAULT_QUESTIONS.each do |idx|
-      question = Question.find(idx)
-      question_text = question.nil? ? '' : question.question_text
-
-      answer = Answer.where(question_id: idx, user: current_user).first
-      answer_text = answer.nil? ? '' : answer.answer_text
-
-      @qna << {
-          question_id: idx,
-          question: question_text,
-          answer: answer_text
-      }
-    end
-
-    render 'index'
-  end
+  # def index
+  #   @questions_list = DEFAULT_QUESTIONS.join(",")
+  #   @qna = []
+  #   DEFAULT_QUESTIONS.each do |idx|
+  #     question = Question.find(idx)
+  #     question_text = question.nil? ? '' : question.question_text
+  #
+  #     answer = Answer.where(question_id: idx, user: current_user).first
+  #     answer_text = answer.nil? ? '' : answer.answer_text
+  #
+  #     @qna << {
+  #         question_id: idx,
+  #         question: question_text,
+  #         answer: answer_text
+  #     }
+  #   end
+  #
+  #   render 'index'
+  # end
 
   def show
     question_number = params[:id].to_i
@@ -30,6 +30,9 @@ class SurveyController < ApplicationController
     @question_text = question.question_text
     answer = Answer.where(question_id: question_number, user: current_user).first
     @answer_text = answer.nil? ? '' : answer.answer_text
+    if question_number == 2
+      @answer_set = answer.nil? ? Set.new() : Set.new(answer.answer_text.split(','))
+    end
 
     @green_rectangles_count = question_number
     @grey_rectangles_count = 5 - question_number
