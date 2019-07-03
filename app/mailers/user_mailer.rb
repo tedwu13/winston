947156ddc450b4a2d57972f4f4a5cfc86a575bbf
Ted Wu <tedwu2016@u.northwestern.edu>
2019-07-03 10:09:06 -0700

    all mailer class

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  default :from => "tedwu2016@u.northwestern.edu"
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
+
+end
