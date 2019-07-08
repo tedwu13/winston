ca06a0af1381308914e99b327b89c05f1f6faaa0
Michael Wang <michaelwang0712@gmail.com>
2019-07-07 23:10:02 -0700

    Refactor survey code

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,37 +1,63 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_student!
+  before_action :validate_question, only: :create
+
+  DEFAULT_QUESTIONS = [1, 2, 3, 4, 5].freeze
+
   QUESTION_2_CHOICES = ['northeast', 'midwest', 'southeast', 'southwest', 'west', 'no_preference']
 
   # POST /answers
   def create
-    question_id = params[:question_id]
-    if question_id.to_i == 2
+    answer = load_answer
+    unless answer.empty?
+      Answer
+        .where(user_id: current_user.id, question_id: @question_id)
+        .first_or_initialize
+        .update!(answer_text: answer)
+    end
+
+    final_question? && mark_survey_completed!
+
+    if final_question?
+      redirect_to dashboard_index_path
+    else
+      redirect_to survey_path(next_question_id)
+    end
+  end
+
+  private
+
+  def load_answer
+    if @question_id == 2
       answer = []
       QUESTION_2_CHOICES.each do |choice|
         if params.key?(choice)
           answer.push(choice)
         end
       end
-      answer = answer.join(',')
+      answer.join(',')
     else
-      answer = params[question_id]
+      params[params[:question_id]]
     end
+  end
 
-    unless answer.empty?
-      saved_answer = Answer.find_by(user_id: current_user.id, question_id: question_id)
-      if saved_answer.present?
-        saved_answer.update(answer_text: answer)
-        saved_answer.save!
-      else
-        Answer.create(user_id: current_user.id, question_id: question_id, answer_text: answer)
-      end
+  def validate_question
+    @question_id = params[:question_id].to_i
+    unless DEFAULT_QUESTIONS.include? @question_id
+      head 404
     end
+  end
 
-    next_question_id = question_id.to_i + 1
+  def final_question?
+    @question_id == DEFAULT_QUESTIONS.last
+  end
 
-    if question_id.to_i == 5
-      redirect_to "/"
-    else
-      redirect_to "/survey/#{next_question_id}"
-    end
+  def mark_survey_completed!
+    current_user.survey_completed = true
+    current_user.save!
+  end
+
+  def next_question_id
+    @question_id + 1
   end
 end
--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,28 +1,35 @@
 class SurveyController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_student!
+  before_action :validate_question, only: :show
+  before_action :load_question_and_answer, only: :show
 
-  DEFAULT_QUESTIONS = [1,2,3,4,5]
+  DEFAULT_QUESTIONS = [1, 2, 3, 4, 5].freeze
 
-  def show
-    question_number = params[:id].to_i
-    unless DEFAULT_QUESTIONS.include? question_number
-      head 404 and return
+  private
+
+  def validate_question
+    @question_number = params[:id].to_i
+    unless DEFAULT_QUESTIONS.include? @question_number
+      head 404
     end
+  end
 
-    question = Question.find(question_number)
-    @question_text = question.question_text
-    answer = Answer.where(question_id: question_number, user: current_user).first
-    @answer_text = answer.nil? ? '' : answer.answer_text
-    if question_number == 2
-      @answer_set = answer.nil? ? Set.new() : Set.new(answer.answer_text.split(','))
-    end
+  def load_question_and_answer
+    question = Question.find(params[:id].to_i)
+    @qna = {
+      id: @question_number,
+      question_text: question.question_text,
+      answer: get_answer
+    }
+  end
 
-    @green_rectangles_count = question_number
-    @grey_rectangles_count = 5 - question_number
-
-    if question_number == 5
-      current_user.survey_completed = true
-      current_user.save!
+  # question 2 has a strangely formatted answer due to its multiple choice nature
+  def get_answer
+    answer = Answer.where(question_id: @question_number, user: current_user).first
+    if @question_number == 2
+      answer.nil? ? Set.new : Set.new(answer&.answer_text&.split(','))
+    else
+      answer.nil? ? '' : answer.answer_text
     end
   end
 end
