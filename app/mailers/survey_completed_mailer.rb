efc68516e1201c163cff25af73b08312e873dedc
Ted Wu <tedwu2016@u.northwestern.edu>
2019-09-25 21:59:46 -0700

    mailer initial

--- app/mailers/survey_completed_mailer.rb
+++ app/mailers/survey_completed_mailer.rb
@@ -3,8 +3,9 @@ class SurveyCompletedMailer < ApplicationMailer
 
   def welcome_email(user)
     attachments.inline["choodry.png"] = File.read("#{Rails.root}/app/assets/images/choodry.png")
+    attachments.inline["email_background.png"] = File.read("#{Rails.root}/app/assets/images/email_background.jpg")
     @user = user
-    @concierge_name = ['Sida Wu', 'Ted Lu', 'Michael Hsu', 'Michelle Wang'].sample
+    @concierge_name = 'Casper Sermsuksan'
     mail(to: @user.email, subject: "#{@user.first_name}, welcome to Winston!")
   end
 end
--- test/mailers/previews/survey_completed_mailer_preview.rb
+++ test/mailers/previews/survey_completed_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/survey_completed_mailer
 class SurveyCompletedMailerPreview < ActionMailer::Preview
-
+  def welcome_email
+    SurveyCompletedMailer.welcome_email(User.first)
+  end
 end
