7ac8ebad5e8fd99ff28612f11e1edc9a70f56420
Ted Wu <tedwu2016@u.northwestern.edu>
2019-09-26 10:51:02 -0700

    finished email

--- app/mailers/survey_completed_mailer.rb
+++ app/mailers/survey_completed_mailer.rb
@@ -4,6 +4,7 @@ class SurveyCompletedMailer < ApplicationMailer
   def welcome_email(user)
     attachments.inline["choodry.png"] = File.read("#{Rails.root}/app/assets/images/choodry.png")
     attachments.inline["email_background.png"] = File.read("#{Rails.root}/app/assets/images/email_background.jpg")
+    attachments.inline["winston_logo.png"] = File.read("#{Rails.root}/app/assets/images/logo_white.png")
     @user = user
     @concierge_name = 'Casper Sermsuksan'
     mail(to: @user.email, subject: "#{@user.first_name}, welcome to Winston!")
