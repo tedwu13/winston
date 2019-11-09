9bacf1c3dcecb1ed339c3962f728ac15d92df8b8
Michael Wang <michaelwang0712@gmail.com>
2019-11-09 00:06:34 -0800

    Updated mailgun config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # mailgun config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'askwinston.org' }
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "askwinston.org",
+    :user_name => "postmaster@mail.askwinston.org",
+    :password => Rails.application.secrets.mailgun_password,
+  }
 end
