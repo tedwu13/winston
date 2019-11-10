88f52908d34924261faa95bac01f2372b95874b2
Michael Wang <michaelwang0712@gmail.com>
2019-11-10 13:23:29 -0800

    Fix email stuff

--- app/mailers/survey_completed_mailer.rb
+++ app/mailers/survey_completed_mailer.rb
@@ -1,10 +1,10 @@
 class SurveyCompletedMailer < ApplicationMailer
-  default from: 'notifications@winston.com'
+  default from: 'hello@mail.askwinston.org'
 
   def welcome_email(user)
     attachments.inline["choodry.png"] = File.read("#{Rails.root}/app/assets/images/choodry.png")
     attachments.inline["email_background.png"] = File.read("#{Rails.root}/app/assets/images/email_background.jpg")
-    attachments.inline["winston_logo.png"] = File.read("#{Rails.root}/app/assets/images/logo_white.png")
+    attachments.inline["winston_logo.png"] = File.read("#{Rails.root}/app/assets/images/logo.png")
     @user = user
     @concierge_name = 'Casper Sermsuksan'
     mail(to: @user.email, subject: "#{@user.first_name}, welcome to Winston!")
