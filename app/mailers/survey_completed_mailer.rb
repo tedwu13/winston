d2e052c7fb2448a7da06097cd48a5ee8a592cb9b
Michael Wang <michaelwang0712@gmail.com>
2019-12-26 00:12:13 -0500

    Fix email copy

--- app/mailers/survey_completed_mailer.rb
+++ app/mailers/survey_completed_mailer.rb
@@ -2,7 +2,9 @@ class SurveyCompletedMailer < ApplicationMailer
   default from: 'hello@mail.askwinston.org'
 
   def welcome_email(user)
-    attachments.inline["choodry.png"] = File.read("#{Rails.root}/app/assets/images/choodry.png")
+    attachments.inline["joyce.png"] = File.read("#{Rails.root}/app/assets/images/joyce.png")
+    attachments.inline["nel.png"] = File.read("#{Rails.root}/app/assets/images/nel.png")
+    attachments.inline["vanessa.png"] = File.read("#{Rails.root}/app/assets/images/vanessa.png")
     attachments.inline["email_background.png"] = File.read("#{Rails.root}/app/assets/images/email_background.jpg")
     attachments.inline["winston_logo.png"] = File.read("#{Rails.root}/app/assets/images/logo.png")
     @user = user
