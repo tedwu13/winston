8097f07543e5a7f14e384c593326bfd454d1342f
Michael Wang <michaelwang0712@gmail.com>
2019-12-28 03:28:12 -0500

    Upgraded email copy and added some major options

--- app/mailers/survey_completed_mailer.rb
+++ app/mailers/survey_completed_mailer.rb
@@ -1,14 +1,12 @@
 class SurveyCompletedMailer < ApplicationMailer
-  default from: 'hello@mail.askwinston.org'
-
   def welcome_email(user)
-    attachments.inline["joyce.png"] = File.read("#{Rails.root}/app/assets/images/joyce.png")
-    attachments.inline["nel.png"] = File.read("#{Rails.root}/app/assets/images/nel.png")
-    attachments.inline["vanessa.png"] = File.read("#{Rails.root}/app/assets/images/vanessa.png")
+    attachments.inline["felisha.png"] = File.read("#{Rails.root}/app/assets/images/felisha.png")
+    attachments.inline["ted.png"] = File.read("#{Rails.root}/app/assets/images/ted.png")
+    attachments.inline["yash.png"] = File.read("#{Rails.root}/app/assets/images/yash.png")
     attachments.inline["email_background.png"] = File.read("#{Rails.root}/app/assets/images/email_background.jpg")
     attachments.inline["winston_logo.png"] = File.read("#{Rails.root}/app/assets/images/logo.png")
     @user = user
     @concierge_name = 'Casper Sermsuksan'
-    mail(to: @user.email, subject: "#{@user.first_name}, welcome to Winston!")
+    mail(to: @user.email, from: 'Winston <hello@mail.askwinston.org>', subject: "#{@user.first_name}, welcome to Winston!")
   end
 end
